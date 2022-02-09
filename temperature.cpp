732a72443db62675bfaad46aa0d68c8063a52fc4
Piotr Pasich <piotr.pasich@gmail.com>
2022-02-09 20:24:03 +0100

    init

---
+++ temperature.cpp
@@ -0,0 +1,44 @@
+#include "temperature.h"
+#include <DHT.h>
+#include <ArduinoJson.h>
+#define MIN_INTERVAL 5000
+
+Temperature::Temperature(uint8_t pin, uint8_t dhtType, String s_id): dht(pin, dhtType), id(s_id)
+{
+ Serial.println("Temperature initialized");
+}
+
+String Temperature::getMessage()
+{
+  digitalWrite(BUILTIN_LED, HIGH);
+  Serial.println("send message");
+  float temperature = dht.readTemperature();
+  float humidity    = dht.readHumidity();
+
+  Serial.print(temperature);
+  Serial.print("|");
+  Serial.println(humidity);
+  StaticJsonDocument<200> document;    
+  document["sensorType"] = "Temperature";
+  document["id"] = id;
+
+  if (!isnan(temperature) && !isnan(humidity)) {    
+    document["temperature"] = temperature;
+    document["humidity"] = humidity;
+  } else {
+    Serial.println("One of values is null");
+  }
+  digitalWrite(BUILTIN_LED, LOW);
+  String msg;
+  serializeJson(document, msg);
+  
+  return msg;
+}
+float Temperature::readTemperature()
+{
+  return dht.readTemperature();
+}
+float Temperature::readHumidity()
+{
+  return dht.readHumidity();
+}
---
+++ temperature.h
@@ -0,0 +1,17 @@
+#ifndef TEMPERATURE_H
+#define TEMPERATURE_H
+#include <DHT.h>
+
+class Temperature 
+{
+  public:
+   Temperature(uint8_t pin, uint8_t dhtType, String id);
+   float readTemperature();
+   float readHumidity();
+   String getMessage();
+
+ private:
+  DHT dht;
+  String id;
+};
+#endif
